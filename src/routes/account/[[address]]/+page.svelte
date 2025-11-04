5de73f97070bbe88b5d1de38faaead92e787268e
code0xff <ian.jungyong.um@gmail.com>
2025-11-05 00:13:13 +0900

    feat: add get storage to account page

--- src/routes/account/[[address]]/+page.svelte
+++ src/routes/account/[[address]]/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import { Button } from '@/components/ui/button';
 	import * as Card from '@/components/ui/card';
+	import { Input } from '@/components/ui/input';
 	import * as Table from '@/components/ui/table';
 	import { DEFAULT_RPC } from '@/constants';
 	import { printNumber, printWei, providerStore } from '@/index';
@@ -15,11 +17,13 @@
 	let nonce: number;
 	let code: string;
 
+	let slot: string;
+	let result: string;
+
 	onMount(async () => {
 		if (!provider) {
 			const rpc = localStorage.getItem('rpc') ?? DEFAULT_RPC;
 			provider = new JsonRpcProvider(rpc);
-
 			providerStore.set(provider);
 		}
 
@@ -27,6 +31,15 @@
 		nonce = await provider.getTransactionCount(data.address);
 		code = await provider.getCode(data.address);
 	});
+
+	async function getStorageAt() {
+		if (!provider) {
+			const rpc = localStorage.getItem('rpc') ?? DEFAULT_RPC;
+			provider = new JsonRpcProvider(rpc);
+			providerStore.set(provider);
+		}
+		result = await provider.getStorage(data.address, slot);
+	}
 </script>
 
 <div>
@@ -49,6 +62,38 @@
 						<Table.Row>
 							<Table.Cell>Code</Table.Cell>
 							<Table.Cell>{code ? code : ''}</Table.Cell>
+						</Table.Row>
+					</Table.Body>
+				</Table.Root>
+			</Card.Content>
+		</Card.Root>
+	</div>
+	<div class="m-4">
+		<Card.Root>
+			<Card.Header>
+				<Card.Title>Storage</Card.Title>
+			</Card.Header>
+			<Card.Content>
+				<Table.Root>
+					<Table.Body>
+						<Table.Row>
+							<Table.Cell>Slot</Table.Cell>
+							<Table.Cell>
+								<div class="flex flex-row gap-4">
+									<div class="w-full">
+										<Input bind:value={slot} />
+									</div>
+									<div>
+										<Button onclick={getStorageAt}>Get</Button>
+									</div>
+								</div>
+							</Table.Cell>
+						</Table.Row>
+						<Table.Row>
+							<Table.Cell>Value</Table.Cell>
+							<Table.Cell>
+								<Input readonly bind:value={result} />
+							</Table.Cell>
 						</Table.Row>
 					</Table.Body>
 				</Table.Root>
