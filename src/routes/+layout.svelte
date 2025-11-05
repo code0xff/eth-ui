22e02721eaf19dedadbe6664980883443cc79cfb
code0xff <ian.jungyong.um@gmail.com>
2025-11-06 00:18:38 +0900

    feat: add title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { ModeWatcher } from 'mode-watcher';
-	import { Toaster } from "$lib/components/ui/sonner/index.js";
+	import { Toaster } from '$lib/components/ui/sonner/index.js';
 
 	let { children } = $props();
 </script>
@@ -14,4 +14,11 @@
 <Toaster />
 
 <ModeWatcher />
+
+<div class="m-4">
+	<h2 class="scroll-m-20 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
+		ETH UI
+	</h2>
+</div>
+<hr />
 {@render children?.()}
