30c43ef13a9d65804d98468d1df9de8fc1f86f4f
code0xff <ian.jungyong.um@gmail.com>
2025-12-02 23:20:53 +0900

    feat: add icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 	import { ModeWatcher, toggleMode } from 'mode-watcher';
 	import { onMount } from 'svelte';
 	import { resolve } from '$app/paths';
