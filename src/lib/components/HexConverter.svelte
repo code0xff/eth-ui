5f1c9b5db744d4bffd906a2027b7a5296ae9158b
code0xff(WM) <ian.jungyong.um@gmail.com>
2026-01-28 17:21:07 +0900

    feat: add applyUint256Hex

--- src/lib/components/HexConverter.svelte
+++ src/lib/components/HexConverter.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import { toBigInt, toBeHex } from 'ethers';
-	import { Calculator } from '@lucide/svelte';
+	import { ArrowRightToLine, Calculator, Clipboard } from '@lucide/svelte';
 	import { Button } from '@/components/ui/button';
 	import * as Dialog from '@/components/ui/dialog';
 	import { Input } from '@/components/ui/input';
@@ -8,23 +8,31 @@
 
 	let open = false;
 	let decimal = '0';
-	let hex = '0x0000000000000000000000000000000000000000000000000000000000000000';
+	let hex = '0x00';
 
 	$: if (open) {
 		decimal = '0';
-		hex = '0x0000000000000000000000000000000000000000000000000000000000000000';
+		hex = '0x00';
 	}
 
-	function toHex(e: Event & { currentTarget: HTMLInputElement }): void {
-		hex = toBeHex(e.currentTarget.value, 32);
+	function onDecimalInput(e: Event & { currentTarget: HTMLInputElement }): void {
+		if (e.currentTarget.value.trim().length === 0) return;
+		hex = toBeHex(e.currentTarget.value);
 	}
 
-	function toDecimal(e: Event & { currentTarget: HTMLInputElement }): void {
+	function onHexInput(e: Event & { currentTarget: HTMLInputElement }): void {
+		if (e.currentTarget.value.trim().length === 0) return;
+
 		let _hex = e.currentTarget.value;
 		if (!_hex.startsWith('0x')) {
 			_hex = `0x${_hex}`;
 		}
 		decimal = toBigInt(_hex).toString();
+	}
+
+	function applyUint256Hex(): void {
+		if (decimal.length === 0) return;
+		hex = toBeHex(decimal, 32);
 	}
 </script>
 
@@ -49,13 +57,23 @@
 							<Table.Row>
 								<Table.Cell>Decimal</Table.Cell>
 								<Table.Cell>
-									<Input placeholder="0" value={decimal} oninput={toHex} />
+									<Input placeholder="0" value={decimal} oninput={onDecimalInput} />
 								</Table.Cell>
 							</Table.Row>
 							<Table.Row>
 								<Table.Cell>Hex</Table.Cell>
 								<Table.Cell>
-									<Input placeholder="0x" value={hex} oninput={toDecimal} />
+									<div class="flex gap-2">
+										<Input placeholder="0x00" value={hex} oninput={onHexInput} />
+										<Button
+											class="cursor-pointer"
+											size="icon"
+											variant="outline"
+											onclick={applyUint256Hex}
+										>
+											<ArrowRightToLine />
+										</Button>
+									</div>
 								</Table.Cell>
 							</Table.Row>
 						</Table.Body>
